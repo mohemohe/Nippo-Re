0823fb0b2cbca7bb1d12d0cbbd0af8728adb49ea
mohemohe <mohemohe@ghippos.net>
2017-11-26 22:26:34 +0900

    PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'nippore',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+};
