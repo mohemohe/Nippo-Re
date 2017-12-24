33920187ebe165204af2530ce7b47ac6a728ac3e
mohemohe <mohemohe@ghippos.net>
2017-12-25 01:24:43 +0900

    リバプロ氏〜〜〜〜〜〜〜〜〜〜

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -16,6 +16,8 @@ module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 
+  sails.hooks.http.app.set('trust proxy', true);
+
   git.long(hash => {
     global.currentGitCommitHash = hash;
     cb();
